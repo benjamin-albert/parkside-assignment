549d229e90045550770e24f7fbd3ac10c541fc35
Benjamin Albert <benjamin_a5@yahoo.com>
2016-12-29 22:49:16 -0800

    Fixes indentation in loanDAO.js

--- loanDAO.js
+++ loanDAO.js
@@ -2,25 +2,24 @@ var BigNumber = require('bignumber.js');
 var Loan = require('./schemas/loan');
 
 function LoanDAO() {
-
 }
 
 LoanDAO.prototype.save = function(input, cb) {
-    // I prefer decimal arithmetic over floating point arithmetic
-    // whenever money is involved.
-    var approved = 
-      new BigNumber(input.amount)
-        .dividedBy(input.value)
-        .gt(0.4);
+  // I prefer decimal arithmetic over floating point arithmetic
+  // whenever money is involved.
+  var approved = 
+    new BigNumber(input.amount)
+      .dividedBy(input.value)
+      .gt(0.4);
 
-    var loan = new Loan({
-      amount: input.amount,
-      value: input.value,
-      ssn: input.ssn,
-      status: approved ? 0 : 1
-    });
+  var loan = new Loan({
+    amount: input.amount,
+    value: input.value,
+    ssn: input.ssn,
+    status: approved ? 0 : 1
+  });
 
-    loan.save(cb);
+  loan.save(cb);
 };
 
 LoanDAO.prototype.find = function(cb) {
